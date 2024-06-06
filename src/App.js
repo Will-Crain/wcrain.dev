3fc73f924bb56eb339532acc5be3cb38af94a3b0
EngineerYo <Crain.Will@proton.me>
2024-06-05 19:15:23 -0700

    Removed some create-react-app boilerplate

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import Home from './components/Home'
 
 function App() {
-  console.log(process.env.REACT_APP_TEST)
   return (
-    < div className="App" >
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div >
-  );
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
+  )
 }
 
 export default App;
