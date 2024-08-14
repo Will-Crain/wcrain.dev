872d3326acb6cd3b9247f8d87c9eb8ccb44e7b70
EngineerYo <Crain.Will@proton.me>
2024-08-13 20:48:57 -0700

    Removed redux

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import './index.css'
 
-import { store } from './app/store';
-import { Provider } from 'react-redux'
 import Root from './routes/Root'
 import Login from './routes/Login'
 import Register from './routes/Register'
