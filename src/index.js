eefde55d57539357c0d5be732bf0b919b892123a
EngineerYo <Crain.Will@proton.me>
2024-08-13 20:48:50 -0700

    Refactored after updating react-router-dom

--- src/index.js
+++ src/index.js
@@ -2,23 +2,39 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 
 import { store } from './app/store';
 import { Provider } from 'react-redux'
+import Root from './routes/Root'
+import Login from './routes/Login'
+import Register from './routes/Register'
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      {
+        path: '/login',
+        element: <Login />
+      },
+      {
+        path: '/register',
+        element: <Register />
+      }
+    ]
+  },
+])
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/*" element={<App />} />
-        </Routes>
-      </BrowserRouter>
-    </Provider >
+    <RouterProvider router={router} />
   </React.StrictMode>
-);
+)
