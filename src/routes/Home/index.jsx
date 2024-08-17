3ab9cfb16b2d29ecd04a17ada21717351594ddbd
EngineerYo <Crain.Will@proton.me>
2024-08-16 19:26:42 -0700

    Added project button

--- src/routes/Home/index.jsx
+++ src/routes/Home/index.jsx
@@ -5,9 +5,9 @@ import { BillTracker } from '../BillTracker'
 
 const Home = () => {
   return (
-    <section className='flex h-screen flex-row items-top justify-left w-full pt-4' id='content'>
-      <Link to='/billTracker' className='w-48 h-48 bg-white rounded-lg shadow-md dark:border dark:bg-gray-800 dark:border-gray-700'>
-
+    <section className='flex h-screen flex-row items-top justify-left w-screen p-4' id='content'>
+      <Link to='/billTracker' className='w-screen h-48 bg-white rounded-lg shadow-md dark:border dark:bg-gray-800 dark:border-gray-700 text-center font-bold text-2xl sm:w-48'>
+        Bill Tracker
       </Link>
     </section>
   )
