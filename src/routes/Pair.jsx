8f69ca0cb5f61bcd10ae4c7e0b8a3d016eb91891
EngineerYo <Crain.Will@proton.me>
2024-09-05 07:56:26 -0700

    Added some helper text in the Pairing screen

--- src/routes/Pair.jsx
+++ src/routes/Pair.jsx
@@ -52,15 +52,19 @@ const Pair = (props) => {
       [name]: value,
     }))
   }
+  let appNameLookup = {
+    'billTracker': 'Bill Tracker',
+    'expenseTracker': 'Shared Expenses Tracker',
+  }
   return (
     <section className='flex h-screen flex-col items-center justify-center w-full px-4 sm:w-[40rem]' id='alert'>
       <div className='w-full bg-white rounded-lg shadow-md dark:border dark:bg-gray-800 dark:border-gray-700'>
         <h1 className='text-center m-2 mb-0 text-xl font-bold text-black md:text-3xl dark:text-white'>
-          Bill Tracker Connection
+          {`${appNameLookup[app]} Connection`}
         </h1>
         <form onSubmit={handleSubmit} className='w-full flex flex-col p-4 gap-4 items-center'>
           {/* <p className='px-2 text-center text-xs h-auto'>This application requires the linking of your account to another user's account. A user you wish to link to can access their UserID on their profile page. Please copy that UserID into the field below, then submit this form. If successful, you will be automatically redirected.</p> */}
-          <p className='px-2 text-center text-xs h-auto'>The application you're trying to access requires the linking of two accounts. The user you wish to link to can access their UserID from their Profile page. You may enter their UserID below. Upon submitting, </p>
+          <p className='px-2 text-center text-xs h-auto'>The application you're trying to access requires the linking of two accounts. The user you wish to link to can access their UserID from their Profile page. You may enter their UserID below. Upon submitting, you shall be redirected and your connected partner may navigate to the App.</p>
 
           <span className='p-1 w-full' />
 
@@ -72,7 +76,7 @@ const Pair = (props) => {
 
         </form>
       </div>
-      {linkStatus && <Navigate to={`/ ${app}`} />}
+      {linkStatus && <Navigate to={`/${app}`} />}
     </section>
   )
 }
